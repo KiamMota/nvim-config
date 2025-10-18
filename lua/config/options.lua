85aa60354161ebbe4da245f81406a87933f6b20b
KiamMota <somentekiam@gmail.com>
2025-10-18 13:01:01 -0300

    add : plugins

---
+++ lua/config/options.lua
@@ -0,0 +1,2 @@
+vim.opt.guicursor = "n-v-c:block"
+vim.g.autoformat = false
---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "tpope/vim-fugitive",
+    keys = {
+      { "<leader>gs", ":Gstatus<CR>", desc = "Git status" },
+      { "<leader>ga", ":Gwrite<CR>", desc = "Git add" },
+      { "<leader>gc", ":Gcommit<CR>", desc = "Git commit" },
+      { "<leader>gp", ":Gpush<CR>", desc = "Git push" },
+    },
+  },
+}
+
